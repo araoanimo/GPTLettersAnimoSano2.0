3ad5fa654807e395d81c7bcd14a4e9718189fb49
Arjun Rao <arao@animosanopsychiatry.com>
2023-02-09 15:41:44 -0500

    changed duplicate prompt as well

--- client/src/App.js
+++ client/src/App.js
@@ -56,7 +56,8 @@ function App() {
     prompt = `I want you to act as a provider writing a letter for their health provider practice. 
     I will give you the name of the provider that you are impersonating and the person 
     or entity that the letter is addressed to. I will also give you the reason for the 
-    letter and an illness that the patient may have. Use these clues to construct a letter in a 
+    letter and an illness that the patient may have. The letter is not addressed to the patient.
+    Use these clues to construct a letter in a 
     professional manner that is about 200 words 
     Provider Name: ${docName}
     Addressed To: ${addressedTo}
